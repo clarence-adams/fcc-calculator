cd26aa19a679890195ac340739313d8b9f9e7a9c
Clarence <clarence.e.a@gmail.com>
2021-02-14 14:11:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [display, setDisplay] = useState("0")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="calculator">
+      <div id="display">{display}</div>
+      <button id="clear">AC</button>
+      <button id="equals">=</button>
+      <div id="numbers">
+        <button id="zero">0</button>
+        <button id="one">1</button>
+        <button id="two">2</button>
+        <button id="three">3</button>
+        <button id="four">4</button>
+        <button id="five">5</button>
+        <button id="six">6</button>
+        <button id="seven">7</button>
+        <button id="eight">8</button>
+        <button id="nine">9</button>
+        <button id="decimal">.</button>
+      </div>
+      <div id="operators">
+        <button id="add">+</button>
+        <button id="subtract">-</button>
+        <button id="multiply">X</button>
+        <button id="divide">/</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
