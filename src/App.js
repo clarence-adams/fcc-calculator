f159e6af002afbcbebd8b820513524a37d6b0d0c
Clarence <clarence.e.a@gmail.com>
2021-02-16 20:40:40 +0000

    all FCC tests passed, minimal styling

--- src/App.js
+++ src/App.js
@@ -50,35 +50,45 @@ function App() {
 }
 // component that handles button rendering and button functions
 function CalcButton(props) {
+  // helper function that returns true if char that is passed is an operator
   const isOperator = (char) =>
   (char === "+" || char === "-" || char === "*" || char === "/")
   ? true
   : false
-
-  const isDecimalPresent = (element, i) => {
-    if (isOperator(element[i])) {
+  // helper function that returns true if another decimal is already present in
+  // the last number that was entered into the currentInput state
+  const isDecimalPresent = (currentInputArray, i) => {
+    if (isOperator(currentInputArray[i])) {
       return false
-    } else if (element[i] === ".") {
+    } else if (currentInputArray[i] === ".") {
       return true
-    } else if (element[i] === 0) {
+    } else if (i == 0) {
       return false
     } else {
-      isDecimalPresent(element, i - 1)
+      return isDecimalPresent(currentInputArray, i - 1)
     }
   }
-
+  // helper function that returns true if the previous char in currentInput
+  // state does not conflict with the char being entered
+  const isOperationPossible = (currentChar, previousChar) => {
+    return (isOperator(previousChar) && currentChar !== "-") ? false
+    : true
+  }
+  // helper function that uses the mathjs library to handle string evaluation
   const calculatorLogic = (arr) => {
     let newArr = arr.join("")
     return evaluate(newArr)
   }
-
+  // function that handles button clicks
   const clickHandler = (event) => {
     if (event.target.value === "AC") {
-      props.setCurrentInput("0")
+      props.setCurrentInput(["0"])
       props.setIsFirstInput(true)
     } else if (event.target.value === "=") {
       props.setCurrentInput(calculatorLogic(props.currentInput))
       props.setIsFirstInput(true)
+      // sets previous input as an array so we can concat in the future if
+      // we need to
       props.setPreviousInput([calculatorLogic(props.currentInput)])
     } else if (props.isFirstInput) {
       if (event.target.value === "0") {
@@ -91,8 +101,19 @@ function CalcButton(props) {
         props.setIsFirstInput(false)
       }
     } else if (event.target.value === "."
-    && isDecimalPresent(props.currentInput, props.currentInput.length - 1) == false) {
-      props.setCurrentInput(props.currentInput.concat([event.target.value]))
+    && isDecimalPresent(props.currentInput, props.currentInput.length - 1)) {
+      //
+    } else if (isOperator(event.target.value) && !isOperationPossible(event.target.value, props.currentInput[props.currentInput.length - 1])) {
+        props.setCurrentInput(() => {
+          let newArr = props.currentInput
+          newArr[newArr.length - 1] = event.target.value
+          if (isOperationPossible(newArr[newArr.length - 1], newArr[newArr.length - 2])) {
+            return newArr
+          } else {
+            newArr.splice(-1, 1)
+            return newArr
+          }
+        })
     } else {
         props.setCurrentInput(props.currentInput.concat([event.target.value]))
       }
